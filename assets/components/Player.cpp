2d113d2e2bb106af9ab160bf4400312a5290037d
PM <82027284+Platforming-Mayhem@users.noreply.github.com>
2026-01-03 23:06:34 +0000

    Stops player from grabbing ledges from below
    -Will need to revisit

--- assets/components/Player.cpp
+++ assets/components/Player.cpp
@@ -203,7 +203,7 @@ namespace K
 	void Player::LedgeGrab() 
 	{
 		K::ContactPoint contactPoint = K::Physics::GetClosestPoint(this->col->GetPosition() + K::Vector3(0.0f, 0.0f, (this->col->GetHeight() / 2.0f) + this->col->GetRadius()), { K::Layer::LayerType::Player });
-		if (contactPoint.normal.magnitude() > 1.0f && this->col->IsHittingWall() && !this->col->IsColliding() && contactPoint.normal.z > 0.0f && !this->isClimbingUp)
+		if (contactPoint.normal.magnitude() > 1.0f && this->col->IsHittingWall() && !this->col->IsColliding() && contactPoint.normal.z > 0.0f && !this->isClimbingUp && contactPoint.position.z > this->col->GetPosition().z + (this->col->GetHeight() / 2.0f))
 		{
 			this->isGrabbing = true;
 			this->cornerX = contactPoint.position.x + this->col->GetRadius() * contactPoint.normal.x;
