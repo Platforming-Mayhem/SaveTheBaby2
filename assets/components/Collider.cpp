8bd3200d0071287a39715b87fbc7cbfd29f17072
PM <82027284+Platforming-Mayhem@users.noreply.github.com>
2026-03-02 22:24:38 +0000

    Collider fall speed now changes based on framerate

--- assets/components/Collider.cpp
+++ assets/components/Collider.cpp
@@ -87,7 +87,7 @@ namespace K
 			}
 			else
 			{
-				*(this->parent->GetTransform()->position) += K::Vector3(0.0f, 0.0f, -this->time * 2.0f);
+				*(this->parent->GetTransform()->position) += K::Vector3(0.0f, 0.0f, -this->time * 2.0f * K::Time::deltaTime() * 60.0f);
 				this->time += K::Time::deltaTime() * 2.0f;
 			}
 			this->collisionResolution = K::Physics::GetCollisionResolution(this);
@@ -105,7 +105,7 @@ namespace K
 			}
 			else
 			{
-				*(this->parent->GetTransform()->position) += K::Vector3(0.0f, 0.0f, -this->time / 7.0f);
+				*(this->parent->GetTransform()->position) += K::Vector3(0.0f, 0.0f, -this->time * 2.0f * K::Time::deltaTime() * 60.0f);
 				this->time += K::Time::deltaTime() * 2.0f;
 			}
 			this->collisionResolution = K::Physics::GetCollisionResolution(this);
@@ -355,7 +355,7 @@ namespace K
 			}
 			else
 			{
-				*this->parent->GetTransform()->position += K::Vector3(0.0f, 0.0f, -this->time);
+				*this->parent->GetTransform()->position += K::Vector3(0.0f, 0.0f, -this->time * K::Time::deltaTime() * 60.0f);
 				this->time += 1.0f / K::Physics::GetFixedTimeStep();
 			}
 			*this->parent->GetTransform()->position += K::Physics::GetCollisionResolution(this);
